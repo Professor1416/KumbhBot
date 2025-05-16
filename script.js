93ad339abde36c0228a0bcc922a5c3560716b93c
Prashant Dasnur <78973641+Professor1416@users.noreply.github.com>
2025-05-16 12:27:56 +0530

    script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function appendMessage(sender, message) {
 }
 
 async function getBotResponse(userMessage) {
-  const API_KEY = "AIzaSyAoF8rYhEPRPHoQXQVXgOpoQov-2LQsLAo";
+  //const API_KEY = "AIzaSyAoF8rYhEPRPHoQXQVXgOpoQov-2LQsLAo";
   const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
   try {
